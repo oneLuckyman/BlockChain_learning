22f3ce2d6fbe1bce4ff6b75e0a65b213055a2458
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-10-25 21:00:13 +0800

    第一个智能合约

---
+++ Chianlink_tutorial/001_SimpleStorage.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// 声明所使用的开源协议，不声明的话有些编译器会报错 
+// pragma solidity 0.8.7;           // 表示只有0.8.7版本的 solidity 可以编译
+// pragma solidity ^0.8.7;          // 表示0.8.7以及以上版本的 solidity 可以编译
+// pragma solidity >=0.8.7 <0.9.0;  // 表示大于等于0.8.7小于0.9.0版本的 solidity 可以编译
+
+contract SimpleStorge {
+    
+}
