f99ad68ba180b1b7bdc7e2b23065c5d24bc10029
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-11-04 20:12:00 +0800

    合约的继承以及合约函数的重载

---
+++ Chainlink_tutorial/013_ExtraStorage.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./013_SimpleStorage.sol";
+
+// 使用 is 声明一个合约继承自另一个合约
+contract ExtraStorage is SimpleStorage {
+    // 使用 override 可以重载父合约中的 virtual 函数
+    function store(uint256 _favoriteNumber) public override {
+        favoriteNumber = _favoriteNumber + 5;
+    }
+}
