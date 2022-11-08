79bd22ebd0179e97dd3d9813afa907af0ac50855
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-11-08 22:51:38 +0800

    新建一个合约进行资金的流动

---
+++ Chainlink_tutorial/021_FundMe.sol
@@ -0,0 +1,17 @@
+// Get funds from users
+// Withdraw funds
+// Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract FundMe {
+
+    function fund() public payable{
+        // Want to be able to set a minimum fund amount in USD
+        // 1. How do we send ETH to this contract?
+        
+    }
+
+    function withdraw(){}
+}
