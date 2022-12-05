5993a31a8ace515722bd11140f8c4771f6375ce5
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-12-05 21:13:51 +0800

    Solidity 中的变量默认值

---
+++ CodeBaoKu/0103_variable_default_value.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SolidityTest {
+    bool public a;  // false
+    int public b;   // 0
+    uint public c;  // 0
+    address public d;   // 0x0000000000000000000000000000000000000000
+    bytes32 public e;   // 0x0000000000000000000000000000000000000000000000000000000000000000
+}
