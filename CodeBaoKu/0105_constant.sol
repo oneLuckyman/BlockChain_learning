18c7e3c262afe01e13f8c4a9c0654343c94f9e09
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-12-07 21:16:19 +0800

    Solidity 中的常量 constant

---
+++ CodeBaoKu/0105_constant.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SolidityTest {
+    uint public constant X = 32**22 + 8;
+    string public constant TEXT = "abc";
+    bytes32 public constant MY_HASH = keccak256("abc");
+}
