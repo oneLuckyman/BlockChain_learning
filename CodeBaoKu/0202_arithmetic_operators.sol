650f5e474273e134aa91a5aa0e711b3dbd2db61c
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-12-09 22:02:28 +0800

    Solidity 算术运算符

---
+++ CodeBaoKu/0202_arithmetic_operators.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract SolidityTest {
+    constructor() {}
+
+    function getResult() public pure returns(uint) {
+        uint a = 1;
+        uint b = 2;
+        // 算术运算
+        uint result = a + b;
+        return result;
+    }
+}
