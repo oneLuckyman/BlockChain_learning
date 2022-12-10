c8d23e84edbec257e4f7ceb9735e5710aace943d
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-12-10 19:49:54 +0800

    Solidity 比较运算符

---
+++ CodeBaoKu/0203_comparison_operator.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract SolidityTest {
+    uint storedData;
+    constructor() {
+        storedData = 10;
+    }
+
+    function getResult() public pure returns(string memory){
+        uint a = 1;     // 局部变量
+        uint b = 2;
+        uint result = a + b;
+        return integerToString(result);
+    }
+
+    function integerToString(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {  // 比较运算符
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+
+        while (j != 0) {    // 比较运算符
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+
+        while (_i != 0) {
+            bstr[k--] = bytes1(uint8(48 + _i % 10));
+            _i /= 10;
+        }
+        return string(bstr);    // 访问局部变量
+    }
+}
