8551fed734fcace5ff5cec8f17f2ef45dd277e31
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-12-21 20:36:38 +0800

    Solidity do while 循环

---
+++ CodeBaoKu/0403_do_while.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract SolidityTest {
+    uint storedData;
+    constructor() {
+        storedData = 10;
+    }
+    function getResult() public pure returns(string memory){
+        uint a = 10;
+        uint b = 2;
+        uint result = a + b;
+        return integerToString(result);
+    }
+    function integerToString(uint _i) internal pure returns (string memory) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+
+        do {    // do while 循环
+            bstr[k--] = byte(uint8(48 + _i % 10));
+            _i /= 10;
+        }
+        while (_i != 0);
+        return string(bstr);
+    }
+}
