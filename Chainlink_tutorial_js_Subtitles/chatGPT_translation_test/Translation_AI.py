c6f7489b75ba081432602c3140cbcdd66c66feee
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-03-25 22:51:59 +0800

    做了进一步的测试，喂了一些例子，增加了一些提示

--- Chainlink_tutorial_js_Subtitles/chatGPT_translation_test/Translation_AI.py
+++ Chainlink_tutorial_js_Subtitles/chatGPT_translation_test/Translation_AI.py
@@ -7,8 +7,8 @@ from itertools import islice
 import pysrt
 
 load_dotenv()
-os.environ["http_proxy"] = os.environ["PROXY_PORT1"]
-os.environ["https_proxy"] = os.environ["PROXY_PORT1"]
+os.environ["http_proxy"] = os.environ["PROXY_PORT2"]
+os.environ["https_proxy"] = os.environ["PROXY_PORT2"]
 openai.api_key = os.environ["OPENAI_API_KEY1"]
 
 # if txt
@@ -16,16 +16,25 @@ openai.api_key = os.environ["OPENAI_API_KEY1"]
 #     lines = [line.strip() for line in islice(f, 0, 50) if line.strip()]
 
 # if srt
-subs = pysrt.open('./P75_6-12.srt')
-lines = [sub.text for sub in subs]
+subs = pysrt.open('./test.srt')
+lines = [str(sub) for sub in subs]
+example_subs = pysrt.open('./example.srt')
+example_lines = [str(sub) for sub in example_subs]
 
-prompt = ''.join(lines[:30])
+example = ''.join(example_lines)
+prompt = ''.join(lines[0:30])
 
 response = openai.ChatCompletion.create(
     model = "gpt-3.5-turbo",
     messages = [
-        {"role": "system", "content": "你是一个 AI 语音翻译助手，通过接收已经识别好的语音材料文本，给出相应的翻译。注意，识别可能存在错误，你可以根据上下文进行合理的推测和修改。"},
-        {"role": "user", "content": "帮我把这一段字幕文件翻译成中文" + prompt}
+        {"role": "system", "content": "你是一个 AI 智能语音翻译助手，通过接收已经识别好的英语语音字幕文本，给出相应的汉语翻译，\
+         注意以下几点：\
+         第一，这些文本都是人说的话而非文章所以风格会略微口语化一些。\
+         第二，这些字幕文本来自于一个关于区块链和智能合约编程的教程，这就是这些文本的话题背景。\
+         第三，识别可能存在错误，你可以根据上下文以及其话题背景进行合理且适当的推测和修改。\
+         这是一个已经完成翻译的字幕文件，你可以参考一下：\n{}\
+         ".format(example)},
+        {"role": "user", "content": "帮我把这一段字幕文件翻译成中文：\n" + prompt}
     ]
 )
 
@@ -36,5 +45,8 @@ print(response.usage)
 # print(result)
 
 result = result.replace("。", "。\n")
-with open('./Output.txt', 'w', encoding='utf-8') as f:
-    f.write(result)
+with open('./Output.txt', 'a', encoding='utf-8') as f:
+    f.write(result + '\n')
+
+with open('./log.txt', 'a', encoding='utf-8') as f:
+    print(response.usage, file=f)
