a038539a7860c02033f8b7c3a9f6aeb9681f6dcc
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-05-09 22:11:09 +0800

    0510 Ganache 介绍

---
+++ Chainlink_tutorial/05/0510_deploy.js
@@ -0,0 +1,12 @@
+async function main() {
+    // compile them in our code
+    // compile them separately
+    // http://127.0.0.1:7545
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    })
