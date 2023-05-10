c6f8a334416a60cb041b77c503cf399942957b4a
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-05-10 22:55:22 +0800

    0509 编译 Solidity

---
+++ Chainlink_tutorial/05/0509_deploy.js
@@ -0,0 +1,11 @@
+async function main() {
+    // compile them in our code
+    // compile them separately
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    })
