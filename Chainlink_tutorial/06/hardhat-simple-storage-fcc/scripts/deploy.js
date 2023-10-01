20bb96ceaea53c06d38a336fb41373c947336029
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-10-01 23:00:32 +0800

    修改错误，添加 hardhat 配置信息中的网络信息

--- Chainlink_tutorial/06/hardhat-simple-storage-fcc/scripts/deploy.js
+++ Chainlink_tutorial/06/hardhat-simple-storage-fcc/scripts/deploy.js
@@ -3,13 +3,16 @@ const {ethers} = require("hardhat")
 
 // async main
 async function main() {
+  const [deployer] = await ethers.getSigners()
+  console.log("Deploying contracts with the account:", deployer.address)
+
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
   console.log("Deploying contract...")
   const simpleStorage = await SimpleStorageFactory.deploy()
   await simpleStorage.waitForDeployment()
   // what's the private key?
   // what's the rpc url?
-  console.log(`Deployed contract to: ${simpleStorage.address}`)
+  console.log(`Deployed contract to: ${simpleStorage.target}`)
 }
 
 // main
