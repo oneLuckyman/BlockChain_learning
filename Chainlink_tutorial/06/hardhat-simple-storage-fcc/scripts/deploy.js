e12c723cc8ba184464a94b71649a22e6bed3cee4
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-10-02 20:43:09 +0800

    在部署脚本中编写合约验证函数

--- Chainlink_tutorial/06/hardhat-simple-storage-fcc/scripts/deploy.js
+++ Chainlink_tutorial/06/hardhat-simple-storage-fcc/scripts/deploy.js
@@ -1,7 +1,8 @@
 // imports
-const {ethers} = require("hardhat")
+const {ethers, run, network} = require("hardhat")
 
 // async main
+
 async function main() {
   const [deployer] = await ethers.getSigners()
   console.log("Deploying contracts with the account:", deployer.address)
@@ -10,9 +11,28 @@ async function main() {
   console.log("Deploying contract...")
   const simpleStorage = await SimpleStorageFactory.deploy()
   await simpleStorage.waitForDeployment()
-  // what's the private key?
-  // what's the rpc url?
   console.log(`Deployed contract to: ${simpleStorage.target}`)
+  // what happens when we deploy to our hardhat network
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deploymentTransaction.wait(6)
+    await verify(simpleStorage.target, [])
+  }
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...")
+  try{
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch(e) {
+    if (e.message.toLowerCase().includes("already verified")){
+      console.log("Already Verified!")
+    } else {
+      console.log(e)
+    }
+  }
 }
 
 // main
