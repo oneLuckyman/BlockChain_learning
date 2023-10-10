e3098d61ef366d1fad75e3c071bdb82d6e2102ca
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-10-10 21:42:00 +0800

    为在测试网上部署做准备，在验证前添加区块等待

--- Chainlink_tutorial/07/hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
+++ Chainlink_tutorial/07/hardhat-fund-me-fcc/deploy/01-deploy-fund-me.js
@@ -35,6 +35,7 @@ module.exports = async ({getNamedAccounts, deployments}) => {
         from: deployer,
         args: args,
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
 
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY){
--- Chainlink_tutorial/07/hardhat-fund-me-fcc/hardhat.config.js
+++ Chainlink_tutorial/07/hardhat-fund-me-fcc/hardhat.config.js
@@ -10,7 +10,6 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   // solidity: "0.8.8",
   solidity: {
@@ -22,6 +21,7 @@ module.exports = {
       url: SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 11155111,
+      blockConfirmations: 6,
     },
     localhost: {
       url: "http://127.0.0.1:8545/",
