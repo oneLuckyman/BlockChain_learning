4d16c5d9d9be23753385ef1e47bdc4b17c1ac6c0
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-10-27 23:36:40 +0800

    继续编写前端的更多功能 fund 8-5

--- Chainlink_tutorial/08/html-fund-me-fcc/index.js
+++ Chainlink_tutorial/08/html-fund-me-fcc/index.js
@@ -1,3 +1,9 @@
+// in node.js
+// require()
+
+// in front-end javascript you can't use require
+// import
+
 async function connect(){
     if (typeof window.ethereum !== "undefined"){
         window.ethereum.request({method: "eth_requestAccounts"})
@@ -6,3 +12,18 @@ async function connect(){
         document.getElementById("connectButton").innerHTML = "Please install metamask!"
     }
 }
+
+async function fund(ethAmount) {
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined"){
+        // provider / connection to the blockchain
+        // signer / wallet / someone with some gas
+        // contract that we are interaction with
+        // ^ ABI & Address
+        
+    }
+}
+
+// fund function
+
+// withdraw
