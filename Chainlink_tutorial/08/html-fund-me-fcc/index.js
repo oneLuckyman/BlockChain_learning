89c17048c5f2c9c11f51df21574b1fcded20ac6e
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-10-28 23:13:52 +0800

    使用 ethers 编写 JS 前端 8-5

--- Chainlink_tutorial/08/html-fund-me-fcc/index.js
+++ Chainlink_tutorial/08/html-fund-me-fcc/index.js
@@ -3,13 +3,21 @@
 
 // in front-end javascript you can't use require
 // import
+import {ethers} from "./ethers-5.6.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+console.log(ethers)
 
 async function connect(){
     if (typeof window.ethereum !== "undefined"){
         window.ethereum.request({method: "eth_requestAccounts"})
-        document.getElementById("connectButton").innerHTML = "Connected!"
+        connectButton.innerHTML = "Connected!"
     } else {
-        document.getElementById("connectButton").innerHTML = "Please install metamask!"
+        connectButton.innerHTML = "Please install metamask!"
     }
 }
 
