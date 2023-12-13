d07972898edc6a9cd263a13b593576c83fd59712
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-12-13 23:43:39 +0800

    9-3 Raffle.sol 准备工作

---
+++ Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
@@ -0,0 +1,5 @@
+// Raffle
+// Enter the lottery (paying some amount)
+// Pick a random winner (verifiably random)
+// Winner to be selected every X minutes -> completly automate
+// Chainlink Oracle -> Randomness, Automated Execution (Chainlink Keeper)
