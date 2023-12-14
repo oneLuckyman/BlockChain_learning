e89eec6e926e6ab66af0f7fc56c65984a0542da1
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-12-14 23:24:18 +0800

    配置 Raffle.sol

--- Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
+++ Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
@@ -3,3 +3,34 @@
 // Pick a random winner (verifiably random)
 // Winner to be selected every X minutes -> completly automate
 // Chainlink Oracle -> Randomness, Automated Execution (Chainlink Keeper)
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+error Raffle__NotEnoughETHEntered();
+
+contract Raffle {
+    /* State Variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+    
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        // require (msg.value > i_entranceFee, "Not enough ETH!")
+        if(msg.value < i_entranceFee){revert Raffle__NotEnoughETHEntered();}
+        s_players.push(payable(msg.sender));
+    }
+
+    // function pickRandominner() {}
+
+    function getEntranceFee() public view returns(uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns(address) {
+        return s_players[index];
+    }
+}
