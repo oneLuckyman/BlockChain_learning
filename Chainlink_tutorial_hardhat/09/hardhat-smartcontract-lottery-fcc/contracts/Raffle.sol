54cbe8fc14671016795dcd0513895edba8b06ff1
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-12-18 23:27:34 +0800

    添加 Chainlink VRF request

--- Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
+++ Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/contracts/Raffle.sol
@@ -8,6 +8,7 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle__NotEnoughETHEntered();
 
@@ -15,12 +16,14 @@ contract Raffle is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
 
     /* Events */
     event RaffleEnter(address indexed player);
     
     constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
     }
 
     function enterRaffle() public payable {
@@ -36,6 +39,13 @@ contract Raffle is VRFConsumerBaseV2 {
         // Request the random number
         // Once we get it, do something with it
         // 2 transaction process 
+        i_vrfCoordinator.requestRandomWords(
+            keyHash, // gasLane
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        )
     }
 
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {}
