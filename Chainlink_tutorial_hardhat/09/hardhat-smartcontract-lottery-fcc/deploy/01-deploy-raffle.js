f650929e528a0b1bec16ed9b367fc37e086135e2
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-12-30 20:45:04 +0800

    9-16 部署 Raffle，chainlink vrf mock

--- Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/deploy/01-deploy-raffle.js
+++ Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/deploy/01-deploy-raffle.js
@@ -1,12 +1,20 @@
 const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
 
 module.exports = async function({getNameAccounts, deployments}) {
     const {deploy, log} = deployments;
     const {deployer} = await getNameAccounts();
+    let vrfCoordinatorV2Address
 
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    }
+
+    const args = []
     const raffle = await deploy("Raffle", {
         from: deployer, 
-        args: [],
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
