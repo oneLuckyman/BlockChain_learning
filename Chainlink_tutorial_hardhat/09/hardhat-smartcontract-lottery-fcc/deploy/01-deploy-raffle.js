4747a2b457204eaf83af33269d278ebdca218a6d
Jia Xinglong <jiaxinglong1996@outlook.com>
2023-12-31 15:56:12 +0800

    9-17 设置部署参数

--- Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/deploy/01-deploy-raffle.js
+++ Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/deploy/01-deploy-raffle.js
@@ -1,17 +1,33 @@
 const { network } = require("hardhat");
-const { developmentChains } = require("../helper-hardhat-config");
+const { developmentChains, networkConfig } = require("../helper-hardhat-config");
+
+const VRF_SUB_FUND_AMOUNT = ethers.parseEther("30")
 
 module.exports = async function({getNameAccounts, deployments}) {
     const {deploy, log} = deployments;
     const {deployer} = await getNameAccounts();
-    let vrfCoordinatorV2Address
+    const {chainId} = network.config.chainId
+    let vrfCoordinatorV2Address, subscriptionId
 
     if (developmentChains.includes(network.name)) {
         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReceipt = await transactionResponse.wait(1)
+        subscriptionId = transactionReceipt.events[0].args.subId
+        // Fund the subscription
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainId]["subscriptionId"]
     }
 
-    const args = []
+    const entranceFee = networkConfig[chainId]["entranceFee"]
+    const gasLane = networkConfig[chainId]["gasLane"]
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+    const interval = networkConfig[chainId]["interval"]
+
+    const args = [vrfCoordinatorV2Address, entranceFee, gasLane, subscriptionId, callbackGasLimit, interval]
     const raffle = await deploy("Raffle", {
         from: deployer, 
         args: args,
