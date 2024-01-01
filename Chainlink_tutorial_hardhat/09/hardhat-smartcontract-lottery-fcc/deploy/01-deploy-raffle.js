cb7505c9fec46201ff807a5990ddc27c4ee392bb
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-01-01 18:17:02 +0800

    9-17 添加验证函数

--- Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/deploy/01-deploy-raffle.js
+++ Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/deploy/01-deploy-raffle.js
@@ -1,5 +1,6 @@
 const { network } = require("hardhat");
 const { developmentChains, networkConfig } = require("../helper-hardhat-config");
+const {verify} = require("../helper-hardhat-config")
 
 const VRF_SUB_FUND_AMOUNT = ethers.parseEther("30")
 
@@ -34,4 +35,13 @@ module.exports = async function({getNameAccounts, deployments}) {
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY){
+        log("Verifying...")
+        await verify(raffle.address, args)
+    }
+
+    log("---------------------------------------")
 }
+
+module.exports.tags = ["all", "raffle"]
