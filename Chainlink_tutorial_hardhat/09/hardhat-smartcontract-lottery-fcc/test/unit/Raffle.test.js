ddb4d2fb47e0d0cca818d1ca971dd2222988bf7d
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-01-02 22:47:12 +0800

    9-18 准备测试

---
+++ Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/test/unit/Raffle.test.js
@@ -0,0 +1,15 @@
+const { network, getNamedAccounts } = require("hardhat")
+const {developmentChains} = require("../../helper-hardhat-config.js")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Raffle Unit Tests", async function (){
+        let raffle, vrfCoordinatorV2Mock
+
+        beforeEach(async function() {
+            const {deployer} = await getNamedAccounts()
+            await deployments.fixture(["all"])
+            raffle = await ethers.getContract("Raffle", deployer)
+            vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+        })
+    })
