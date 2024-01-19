80111eb847f7b481c83d5e7137dc2ac1c4209158
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-01-19 23:53:31 +0800

    10-5 继续 ManualHeader

--- Chainlink_tutorial_hardhat/10/nextjs-smartcontract-lottery-fcc/components/ManualHeader.jsx
+++ Chainlink_tutorial_hardhat/10/nextjs-smartcontract-lottery-fcc/components/ManualHeader.jsx
@@ -2,7 +2,9 @@ import { useMoralis } from "react-moralis"
 
 export default function ManualHeader() {
 
-    const {enableWeb3} = useMoralis()
+    const {enableWeb3, account} = useMoralis()
 
-    return(<div>Hi from Header!</div>)
+    return(<div>
+        {account ? (<div>Connected to {account.slice(0,6)}...{account.slice(account.length - 4)}</div>) : (<button onClick={async () => {await enableWeb3()}}>Connect</button>)}
+    </div>)
 }
