9f1d9336b9287f5e1e9e013dcc26d05bc4e2b80a
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-01-20 23:54:34 +0800

    10-6 useEffect Hook

--- Chainlink_tutorial_hardhat/10/nextjs-smartcontract-lottery-fcc/components/ManualHeader.jsx
+++ Chainlink_tutorial_hardhat/10/nextjs-smartcontract-lottery-fcc/components/ManualHeader.jsx
@@ -1,8 +1,14 @@
 import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
 
 export default function ManualHeader() {
 
-    const {enableWeb3, account} = useMoralis()
+    const {enableWeb3, account, isWeb3Enabled} = useMoralis()
+
+    useEffect(() => {
+        console.log("HI!")
+        console.log(isWeb3Enabled)
+    }, [isWeb3Enabled])
 
     return(<div>
         {account ? (<div>Connected to {account.slice(0,6)}...{account.slice(account.length - 4)}</div>) : (<button onClick={async () => {await enableWeb3()}}>Connect</button>)}
