6b5c477917119a110c1f2f52a413b9a9461762e9
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-01-24 23:37:51 +0800

    10-10 使用集成的 web3uikit 库替代 ManualHeader

---
+++ Chainlink_tutorial_hardhat/10/nextjs-smartcontract-lottery-fcc/components/Header.js
@@ -0,0 +1,8 @@
+import { ConnectButton } from "web3uikit";
+
+export default function Header() {
+    return (<div>
+        Decentralized Lottery
+        <ConnectButton moralisAuth={false}/>
+    </div>)
+}
