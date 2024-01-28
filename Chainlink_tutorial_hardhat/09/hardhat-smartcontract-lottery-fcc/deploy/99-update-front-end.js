bc56f98916be468c76cac89859f3bb6372d99823
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-01-28 23:58:51 +0800

    10-11 自动化常数值 UI 更新

--- Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/deploy/99-update-front-end.js
+++ Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/deploy/99-update-front-end.js
@@ -1,3 +1,9 @@
+const {ethers} = require("hardhat")
+const fs = require("fs")
+
+const FRONT_END_ADDRESSES_FILE = "../../10/nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
+const FRONT_END_ABI_FILE = "../../10/nextjs-smartcontract-lottery-fcc/constants/abi.json"
+
 module.export = async function () {
     if(process.env.UPDATE_FRONT_END) {
         console.log("Updating front end")
@@ -7,4 +13,5 @@ module.export = async function () {
 
 async function updateContractAddresses() {
     const raffle = await ethers.getContract("Raffle")
+    const currentAddresses = JSON.parse(fs.readFileSync(FRONT_END_ADDRESSES_FILE))
 }
