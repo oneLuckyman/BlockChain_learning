386873dad26ddc81fd0d4f91f2fbad0e5b590924
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-01-29 23:36:56 +0800

    10-11 更新合约地址的函数

--- Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/deploy/99-update-front-end.js
+++ Chainlink_tutorial_hardhat/09/hardhat-smartcontract-lottery-fcc/deploy/99-update-front-end.js
@@ -1,4 +1,4 @@
-const {ethers} = require("hardhat")
+const {ethers, network} = require("hardhat")
 const fs = require("fs")
 
 const FRONT_END_ADDRESSES_FILE = "../../10/nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json"
@@ -13,5 +13,18 @@ module.export = async function () {
 
 async function updateContractAddresses() {
     const raffle = await ethers.getContract("Raffle")
+    const chainId = network.config.chainId.toString()
     const currentAddresses = JSON.parse(fs.readFileSync(FRONT_END_ADDRESSES_FILE))
+    if (chainId in contractAddress) {
+        if (!contractAddress[chainId].includes(raffle.address)) {
+            currentAddresses[chainId].push(raffle.address)
+        } 
+        {
+            currentAddresses[chainId] = [raffle.address]
+        }
+        fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(currentAddresses))
+
+    }
 }
+
+module.exports.tags = ["all", "frontend"]
