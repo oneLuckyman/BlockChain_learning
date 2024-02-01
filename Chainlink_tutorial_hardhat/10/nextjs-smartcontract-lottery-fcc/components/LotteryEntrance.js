91da9bcefb0114c21ae386d5970cbe5680a2930a
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-02-01 23:59:00 +0800

    10-12 运行合约函数

--- Chainlink_tutorial_hardhat/10/nextjs-smartcontract-lottery-fcc/components/LotteryEntrance.js
+++ Chainlink_tutorial_hardhat/10/nextjs-smartcontract-lottery-fcc/components/LotteryEntrance.js
@@ -2,16 +2,36 @@
 import { useWeb3Contract } from "react-moralis"
 import {abi, contractAddresses} from "../constants"
 import { useMoralis } from "react-moralis"
+import { useEffect } from "react"
 
 export default function LotteryEntrace() {
     const {chainId: chainIdHex} = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainIdHex in contractAddresses ? contractAddresses[chainId][0] : null
     
     const {runContractFunction: enterRaffle} = useWeb3Contract({
         abi: abi,
-        contractAddress: contractAddresses,
+        contractAddress: raffleAddress,
         functionName: "enterRaffle",
         params: {},
         msgValue: ,
     })
+
+    const {runContractFunction: getEntranceFee} = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            async function updateUI() {
+                const entranceFeeFromContract = await getEntranceFee()
+            }
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
     return <div>Hi from lottery entrance!</div>
 }
