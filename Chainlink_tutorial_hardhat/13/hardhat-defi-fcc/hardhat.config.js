036fa9d3e959dac746ce5f4ab12e90161870ad49
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-02-23 23:54:48 +0800

    13-6 存储进 Aave

--- Chainlink_tutorial_hardhat/13/hardhat-defi-fcc/hardhat.config.js
+++ Chainlink_tutorial_hardhat/13/hardhat-defi-fcc/hardhat.config.js
@@ -16,7 +16,12 @@ const MATNNET_RPC_URL = process.env.MATNNET_RPC_URL || "key"
 module.exports = {
   // solidity: "0.8.7",
   solidity: {
-    compilers: [{version: "0.8.7"}, {version: "0.6.6"}, {version: "0.4.19"}]
+    compilers: [
+      {version: "0.8.7"}, 
+      {version: "0.6.6"}, 
+      {version: "0.6.12"}, 
+      {version: "0.4.19"}
+    ]
   },
   defaultNetwork: "hardhat",
   networks: {
--- Chainlink_tutorial_hardhat/13/hardhat-defi-fcc/scripts/aaveBorrow.js
+++ Chainlink_tutorial_hardhat/13/hardhat-defi-fcc/scripts/aaveBorrow.js
@@ -1,10 +1,16 @@
-const { getNamedAccounts } = require("hardhat")
+const { getNamedAccounts, ethers } = require("hardhat")
 const { getWeth } = require("../scripts/getWeth")
 
 async function main(){
     // the protocol treats everything as an ERC20 token
     await getWeth()
     const { deployer } = await getNamedAccounts()
+}
+
+async function getLendingPool(account) {
+    const lendingPoolAddressProvider = await ethers.getContractAt("ILendingPoolAddressesProvider",
+     "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+     account)
 }
 
 main()
