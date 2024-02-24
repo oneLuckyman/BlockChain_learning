7dcb966350d0b87ec3f99efc61211e9fcb461111
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-02-24 23:47:35 +0800

    13-6 向 Aave 存储

--- Chainlink_tutorial_hardhat/13/hardhat-defi-fcc/scripts/aaveBorrow.js
+++ Chainlink_tutorial_hardhat/13/hardhat-defi-fcc/scripts/aaveBorrow.js
@@ -5,12 +5,17 @@ async function main(){
     // the protocol treats everything as an ERC20 token
     await getWeth()
     const { deployer } = await getNamedAccounts()
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`LendingPool address ${lendingPool.address}`)
 }
 
 async function getLendingPool(account) {
     const lendingPoolAddressProvider = await ethers.getContractAt("ILendingPoolAddressesProvider",
      "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
      account)
+    const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
+
 }
 
 main()
