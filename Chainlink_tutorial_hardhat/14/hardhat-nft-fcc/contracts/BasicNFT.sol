b0380adb737ac94f44c0051adee2d01d4486fa42
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-03-15 23:34:46 +0800

    14-3 NFT 合约

--- Chainlink_tutorial_hardhat/14/hardhat-nft-fcc/contracts/BasicNFT.sol
+++ Chainlink_tutorial_hardhat/14/hardhat-nft-fcc/contracts/BasicNFT.sol
@@ -3,6 +3,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 pragma solidity ^0.8.7;
 
-contract BasicNft {
-    
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public returns (uint256) {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+    }
 }
