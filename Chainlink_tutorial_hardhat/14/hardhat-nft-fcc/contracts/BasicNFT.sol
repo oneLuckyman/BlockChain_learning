1de43d9e6ecea692fe0ae8842a7b96808d5f93c3
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-03-17 23:49:40 +0800

    14-3 指定 NFT 的 IPFS URL

--- Chainlink_tutorial_hardhat/14/hardhat-nft-fcc/contracts/BasicNFT.sol
+++ Chainlink_tutorial_hardhat/14/hardhat-nft-fcc/contracts/BasicNFT.sol
@@ -4,6 +4,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 pragma solidity ^0.8.7;
 
 contract BasicNft is ERC721 {
+    string public constant TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
     uint256 private s_tokenCounter;
     constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
