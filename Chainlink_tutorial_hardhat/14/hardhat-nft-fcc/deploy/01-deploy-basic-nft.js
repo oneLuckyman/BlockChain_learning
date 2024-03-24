68ab504486de49a97f827e715f261975544d5787
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-03-24 21:12:43 +0800

    14-3 部署脚本

--- Chainlink_tutorial_hardhat/14/hardhat-nft-fcc/deploy/01-deploy-basic-nft.js
+++ Chainlink_tutorial_hardhat/14/hardhat-nft-fcc/deploy/01-deploy-basic-nft.js
@@ -1,2 +1,10 @@
 const { network } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function({getNamedAccounts, developments}) {
+    const {deploy, log} = deployments
+    const {deployer} = await getNamedAccounts()
+
+    log("-----------------------------")
+}
