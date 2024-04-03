17a2021a2b58b690b83e19e003f3c09e5783d6e1
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-04-03 21:04:10 +0800

    14-4 IPFS random nft

--- Chainlink_tutorial_hardhat/14/hardhat-nft-fcc/contracts/RandomipfsNft.sol
+++ Chainlink_tutorial_hardhat/14/hardhat-nft-fcc/contracts/RandomipfsNft.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract RandomIpfsNft {
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract RandomIpfsNft is VRFConsumerBaseV2 {
     
 
     function requestNft() public {}
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal {}
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {}
 
     function tokenURI(uint256) public {}
+
+
 }
