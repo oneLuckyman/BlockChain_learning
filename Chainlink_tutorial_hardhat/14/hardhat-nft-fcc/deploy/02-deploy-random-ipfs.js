e20b198d72c0c8811a1a3db9e150e1b5a9ad34b9
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-04-18 23:20:12 +0800

    14-10 deploy 脚本

---
+++ Chainlink_tutorial_hardhat/14/hardhat-nft-fcc/deploy/02-deploy-random-ipfs.js
@@ -0,0 +1,30 @@
+const { network } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function({getNamedAccounts, developments}) {
+    const {deploy, log} = deployments
+    const {deployer} = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let vrfCoordinatorV2Address, subscriptionId
+
+    if(developmentChains.includes(network.name)){
+        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorMock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+        const tx = await vrfCoordinatorV2Mock.createSubscription()
+        const txReceipt = await tx.wait(1)
+        subscriptionId = txReceipt.events[0].args.subId
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2
+        subscriptionId = networkConfig[chainId].subscriptionId
+    }
+
+    log("---------------------------------")
+    const args = [
+        vrfCoordinatorV2Address,
+        subscriptionId,
+        networkConfig[chainId].gasLane,
+        networkConfig[chainId].mintFee,
+        networkConfig[chainId].callbackGasLimit]
+}
