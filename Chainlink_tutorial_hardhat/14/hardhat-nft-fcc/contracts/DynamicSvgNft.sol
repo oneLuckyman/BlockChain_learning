bcac3d2dc9e9335e403aa92cbba148ecd630ccdb
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-05-02 23:59:32 +0800

    14-17 动态 nft

---
+++ Chainlink_tutorial_hardhat/14/hardhat-nft-fcc/contracts/DynamicSvgNft.sol
@@ -0,0 +1,14 @@
+// DynamicSvgNft.sol
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract DynamicSvgNft {
+    
+    // mint
+    // store out SVG information somewhere
+    // Some logic to say "Show X Image" or "Show Y Image"
+
+}
