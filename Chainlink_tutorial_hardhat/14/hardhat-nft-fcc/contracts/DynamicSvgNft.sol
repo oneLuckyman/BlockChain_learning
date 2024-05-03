9e2561d91087136021375ca042310a5211ac0b4c
Jia Xinglong <jiaxinglong1996@outlook.com>
2024-05-03 22:28:57 +0800

    14-17 动态 SVG nft

--- Chainlink_tutorial_hardhat/14/hardhat-nft-fcc/contracts/DynamicSvgNft.sol
+++ Chainlink_tutorial_hardhat/14/hardhat-nft-fcc/contracts/DynamicSvgNft.sol
@@ -5,10 +5,21 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract DynamicSvgNft {
+contract DynamicSvgNft is ERC721 {
     
     // mint
     // store out SVG information somewhere
     // Some logic to say "Show X Image" or "Show Y Image"
+
+    uint256 private s_tokenCounter;
+
+    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+    }
 
 }
